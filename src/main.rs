d0a470354dac89e64ce8740c22a6824120f31313
Amine ElQaraoui <finetuned@utxo.dev>
2024-03-11 21:38:09 +0100

    added opcodes and address struct. address strcut is yet to be finished.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
-use std::{convert::TryFrom};
+use std::convert::TryFrom;
+use std::str::FromStr;
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Input {
@@ -186,6 +187,77 @@ impl BitcoinTransaction {
         array.append(&mut self.locktime.to_le_bytes().to_vec());
 
         hex::encode(array)
+
+    }
+
+}
+
+fn find_bech32_prefix(bech32: &str) -> &str {
+    // Split at the last occurrence of the separator character '1'.
+    match bech32.rfind('1') {
+        None => bech32,
+        Some(sep) => bech32.split_at(sep).0,
+    }
+}
+
+#[non_exhaustive]
+pub enum BitcoinNetwork {
+    Bitcoin,
+    Testnet,
+    Signet,
+    Regtest,
+}
+
+pub struct BitcoinAddress {
+    pub network: BitcoinNetwork,
+    pub payload: Vec<u8>
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseError;
+
+impl FromStr for BitcoinAddress {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        
+        let bech32_network = match find_bech32_prefix(s) {
+            "bc" | "BC" => Some(BitcoinNetwork::Bitcoin),
+            "tb" | "TB" => Some(BitcoinNetwork::Testnet),
+            "bcrt" | "BCRT" => Some(BitcoinNetwork::Regtest),
+            _ => None,
+        };
+        if let Some(network) = bech32_network {
+            let (_hrp, _version, data) = bech32::segwit::decode(s).unwrap();
+
+            return Ok(BitcoinAddress {
+                network, 
+                payload: data
+            });
+        }
+
+        if s.len() > 50 {
+            panic!("base58 invalid length")
+        }
+        let data = base58::decode_check(s).unwrap();
+        if data.len() != 21 {
+            panic!("base58 invalid length")
+        }
+        
+        let (network, payload) = match data[0] {
+            PUBKEY_ADDRESS_PREFIX_MAIN =>
+                (BitcoinNetwork::Bitcoin, Payload::PubkeyHash(PubkeyHash::from_slice(&data[1..]).unwrap())),
+            SCRIPT_ADDRESS_PREFIX_MAIN =>
+                (BitcoinNetwork::Bitcoin, Payload::ScriptHash(ScriptHash::from_slice(&data[1..]).unwrap())),
+            PUBKEY_ADDRESS_PREFIX_TEST =>
+                (BitcoinNetwork::Testnet, Payload::PubkeyHash(PubkeyHash::from_slice(&data[1..]).unwrap())),
+            SCRIPT_ADDRESS_PREFIX_TEST =>
+                (BitcoinNetwork::Testnet, Payload::ScriptHash(ScriptHash::from_slice(&data[1..]).unwrap())),
+            x => panic!("base58 invalid address version"),
+        };
+        
+
+        Ok(BitcoinAddress { network, payload })
 
     }
 
